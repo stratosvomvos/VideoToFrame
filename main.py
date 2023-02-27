9911aca44845a9cf25267d25c669f9f5d3f851ec
stratosvomvos <99275051+stratosvomvos@users.noreply.github.com>
2023-02-27 15:05:15 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import cv2
+vidcap = cv2.VideoCapture('in.mp4')
+success,image = vidcap.read()
+count = 0
+success = True
+while success:
+  success,image = vidcap.read()
+  cv2.imwrite("%d.jpg" % count, image)     # save frame as JPEG file
+  if cv2.waitKey(10) == 27:                     # exit if Escape is hit
+      break
+  count += 1
